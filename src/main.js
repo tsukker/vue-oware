341c115aa04647d56bda849caadbec8136a06bf9
tsukker <tsukker@users.noreply.github.com>
2020-06-28 16:08:38 +0900

    Prepare for vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import firebase from 'firebase'
 import { firebaseConfig } from './firebaseConfig.js'  // See README.md
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -12,6 +13,7 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 new Vue({
+  store,
   render: h => h(App),
   mounted: function () {
     document.title = "Oware Online";
